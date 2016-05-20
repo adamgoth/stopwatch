a3051df831102a04aede3427db1aa603a108b586
Adam Goth <adamgoth@gmail.com>
2016-05-20 09:29:01 -0600

    Adds repository

---
+++ index.ios.js
@@ -0,0 +1,107 @@
+import formatTime from 'minutes-seconds-milliseconds';
+import React from 'react';
+import {
+  Text,
+  View,
+  TouchableHighlight,
+  TouchableOpacity,
+  AppRegistry,
+  StyleSheet
+} from 'react-native';
+
+var StopWatch = React.createClass({
+  getInitialState: function() {
+    return {
+      timeElapsed: null
+    }
+  },
+  render: function() {
+    return <View style={styles.container}>
+      <View style={[styles.header]}>
+        <View style={[styles.timerWrapper]}>
+          <Text style={styles.timer}>
+            {formatTime(this.state.timeElapsed)}
+          </Text>
+        </View>
+        <View style={[styles.buttonWrapper]}>
+          {this.startStopButton()}
+          {this.lapButton()}
+        </View>
+      </View>
+
+      <View style={[styles.footer]}>
+        <Text>
+          I am a list of Laps
+        </Text>
+      </View>
+    </View>
+  },
+  startStopButton: function() {
+    return <TouchableHighlight
+    underlayColor='gray'
+    onPress={this.handleStartPress}
+    style={[styles.button, styles.startButton]}
+    >
+      <Text>
+        Start
+      </Text>
+    </TouchableHighlight>
+  },
+  lapButton: function() {
+    return <View style={styles.button}>
+      <Text>
+        Lap
+      </Text>
+    </View>
+  },
+  handleStartPress: function() {
+    var startTime = new Date();
+
+    setInterval(() => {
+      this.setState({
+        timeElapsed: new Date() - startTime
+      });
+    }, 30);
+
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'stretch'
+  },
+  header: {
+    flex: 1
+  },
+  footer: {
+    flex: 1
+  },
+  timerWrapper: {
+    flex: 5,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonWrapper: {
+    flex: 3,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  timer: {
+    fontSize: 60
+  },
+  button: {
+    borderWidth: 2,
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  startButton: {
+    borderColor: '#00CC00'
+  }
+});
+
+AppRegistry.registerComponent('stopwatch', () => StopWatch);
